6fa5d9a96be5176f189bf77eedec2b6ccfec05fd
Mohamed Darwish <mu.darwish23@gmail.com>
2019-07-17 12:07:53 +0200

    Added Post Model with published manager, implemented PostList based on Django class based view, and requirements.txt

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.utils import timezone
+
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super(PublishManager, self)
+            .get_queryset()
+            .filter(status='published')
+            )
+
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    ]
+
+    title = models.CharField(max_lenght=250)
+    slug = models.SlugField(
+        max_length=250,
+        allow_unicode=True,
+        unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft')
+    readtime = models.IntegerField(editable=False)
+    objects = models.Manager()
+    published = PublishManager()
+
+    class Meta:
+        ordering = ('-published')
+        db_table = ('blog_post')
+
+    def get_absolute_url(self):
+        pass
