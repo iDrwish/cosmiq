374920210461367edd4d15676d73fa1c0dfae84a
Mohamed Darwish <mu.darwish23@gmail.com>
2019-07-24 16:56:55 +0200

    registered admin model for Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = (
+        'title', 'slug', 'author', 'body',
+        'publish', 'status', 'readtime'
+        )
+    list_display = fields
+    prepopulated_fields = {'slug': ('title', )}
+    list_filter = ('status', 'created', 'publish', 'author')
+    date_hierarchy = ('created')
+    raw_id_fields = ('author', )
+    search_fields = ('title', 'body')
+
+
+admin.site.register(Post, PostAdmin)
