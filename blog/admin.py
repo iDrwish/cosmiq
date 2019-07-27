9144b84cc5a39995356a9709e0c71391564215f1
Mohamed Darwish <mu.darwish23@gmail.com>
2019-07-28 00:57:41 +0200

    Added Tags

--- blog/admin.py
+++ blog/admin.py
@@ -7,14 +7,20 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     fields = (
         'title', 'slug', 'author', 'body',
-        'publish', 'status', 'readtime'
+        'publish', 'status', 'readtime', 'tag_list'
         )
     list_display = fields
     prepopulated_fields = {'slug': ('title', )}
-    list_filter = ('status', 'created', 'publish', 'author')
+    list_filter = ('status', 'created', 'publish', 'author', 'tags')
     date_hierarchy = ('created')
     raw_id_fields = ('author', )
     search_fields = ('title', 'body')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
 
 
 admin.site.register(Post, PostAdmin)
