55e2101fdb167005f0784fc638bc0e50c3cd5dc6
Mohamed Darwish <mu.darwish23@gmail.com>
2019-07-30 16:50:06 +0200

    Added Similar Posts to post_detail view by overwriting the get function

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 from taggit.models import Tag
 from .models import Post
 
@@ -23,8 +24,24 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'blog/Post/post_detail.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(PostDetail, self).get_context_data(**kwargs)
-        # TODO: Add Similar Posts via tags
-        # Add Comments
-        return context
+    def get(self, request, *args, **kwargs):
+        """
+        Rewrite of the get function to add the similar posts
+        to the render function.
+
+        Similar functions by Tag and ordered by Count and Date
+        """
+        self.object = self.get_object()
+        post_tags = self.object.tags.values_list('id', flat=True)
+        similar_posts = (
+            Post.published
+            .filter(tags__in=post_tags)
+            .exclude(id=self.object.id))
+        similar_posts = (
+            similar_posts
+            .annotate(same_tags=Count('tags'))
+            .order_by('-same_tags', '-publish'))[:4]
+        return self.render_to_response({
+            'post': self.object,
+            'similar_posts': similar_posts
+        })
