24389ca71dd9a8d42998f2f33af1e4f79cc72384
Mohamed Darwish <mu.darwish23@gmail.com>
2019-07-30 16:51:42 +0200

    changed get_queryset in PostList

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ from .models import Post
 # Create your views here.
 class PostList(ListView):
     model = Post
-    # queryset = Post.published.all()
     context_object_name = 'blog_posts'
     template_name = 'blog/Post/post_list.html'
 
