952637e987703327e369c746be3b5acd22f392de
Mohamed Darwish <mu.darwish23@gmail.com>
2019-10-08 17:04:10 +0200

    Sites bug fix

--- cosmiq/settings/base.py
+++ cosmiq/settings/base.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = ['www.cosmiq.io', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,4 +143,5 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+SITE_ID = 1
 django_heroku.settings(locals())
