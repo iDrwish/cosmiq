aa161c95cd3d8e75556caf6739a73652a90631cf
Mohamed Darwish <mu.darwish23@gmail.com>
2019-10-09 19:10:00 +0200

    testing slack webhook

--- slack/views.py
+++ slack/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 # Create your views here.
+
+class UpdateMessage(APIView):
+    def post(self, request, format=None):
+        data = JSONParser().parse(request)
+        payload_string = data['payload']
+        print(type(payload_string))
+        print(payload_string)
+        # payl
+        print(data)
