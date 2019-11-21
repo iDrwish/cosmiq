0f39be606832ca0a39ee18bc9a239602b96447d6
Mohamed Darwish <mu.darwish23@gmail.com>
2019-11-21 15:14:55 +0200

    changed URL name

--- misc/api/urls.py
+++ misc/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('slack/update_message', views.UpdateMessage.as_view(), name='slack_update_message'),
-    path('get_os', views.getOS, name='get_os')
+    path('halan', views.getOS, name='get_os')
 ]
